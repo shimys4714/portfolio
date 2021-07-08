a17eebf53ad3d90851eb3039ff0d472e3ae93ef7
shimys4714 <shimys4714@gmail.com>
2021-07-08 11:00:46 +0900

    gnb 생성

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,10 @@ btnMo.onclick = function(e) {
     btnMo.classList.toggle("on");
     menuMo.classList.toggle("on");
 }
+
+$("#gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show();
+});
+$("#gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide();
+});
