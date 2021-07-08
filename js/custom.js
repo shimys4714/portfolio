a1284659d2c8f8b3d7a09f8a28939f97eb08117e
shimys4714 <shimys4714@gmail.com>
2021-07-08 12:09:32 +0900

    gnb 포커스 이벤트 연결

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,22 @@ btnMo.onclick = function(e) {
     menuMo.classList.toggle("on");
 }
 
-$("#gnb>li").on("mouseenter", function(){
+//gnb 활성화
+$(".gnb>li").on("mouseenter", function(){
     $(this).find(".sub").show();
 });
-$("#gnb>li").on("mouseleave", function(){
+$(".gnb>li").on("mouseleave", function(){
     $(this).find(".sub").hide();
+});
+
+//gnb 포커스 활성화
+$(".gnb>li").each(function(index){
+    //1depth1에 첫번째 a요소에 포커스인 이벤트 연결
+    $(".gnb>li").eq(index).find("a").first().on("focusin", function(){
+        $(".gnb>li").eq(index).find(".sub").show(); 
+    });     
+    //length li에 마지막 a요소에 fouseout이벤트 연결
+    $(".gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $(".gnb>li").eq(index).find(".sub").hide(); 
+    });
 });
