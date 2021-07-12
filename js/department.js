87dbc0d822c2f8c5fcec6c2fcc61165ca4440971
shimys4714 <shimys4714@gmail.com>
2021-07-12 23:00:06 +0900

    department content

---
+++ js/department.js
@@ -0,0 +1,11 @@
+
+const frame = document.querySelector("#team");
+const lists = frame.querySelectorAll("article");
+const len = lists.length;
+let num = 0;
+
+for(let i=0; i<len; i++){
+    let pic = lists[i].querySelector(".pic");
+    
+    pic.style.backgroundImage = `url(../img/2_team/team${i+1}.jpg)`;
+}
