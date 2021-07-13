f1fdb939a28fb5358ad3b3c9f6953b73aea038e7
shimys4714 <shimys4714@gmail.com>
2021-07-13 09:24:55 +0900

    Team js 수정

--- js/department.js
+++ js/department.js
@@ -7,5 +7,5 @@ let num = 0;
 for(let i=0; i<len; i++){
     let pic = lists[i].querySelector(".pic");
     
-    pic.style.backgroundImage = `url(../img/2_team/team${i+1}.jpg)`;
+    pic.style.backgroundImage = `url(img/2_team/team${i+1}.jpg)`;
 }
